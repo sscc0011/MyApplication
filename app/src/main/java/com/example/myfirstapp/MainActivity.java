546344abefc014fd0c6350d6ae05c6a019637da0
Administrator <shencong0000@163.com>
2017-01-03 09:54:46 +0800

    1

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -4,13 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static String ma="22222";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         /**
-         *11111111
+         *11111111222123123
          */
 
     }
